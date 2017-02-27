03778a754fd27049d0a2c6a2747ac3f87adb9bbe
koneswaranp <pratheepa.koneswaran@ynov.com>
2017-02-27 14:44:45 +0100

    saisie nombre joueur

--- Nombre_Mot_Magique/src/com/Main.java
+++ Nombre_Mot_Magique/src/com/Main.java
@@ -6,10 +6,13 @@ public class Main {
 	public static void main(String[] args) {
 		//Saisie du nombre magique à trouver
 		Scanner saisie1 = new Scanner(System.in);
-		System.out.println("Veuillez saisir un nombre : ");
+		System.out.println("Veuillez saisir un nombre magique: ");
 		int nombreMagique = saisie1.nextInt();
+	// permet au joueur de saisir des nombres
+		Scanner saisie2 = new Scanner(System.in);
+		System.out.println("Veuillez saisir un nombre :");
+		int nombreSaisi = saisie2.nextInt();
 		
 	}
-	
 	
 }
