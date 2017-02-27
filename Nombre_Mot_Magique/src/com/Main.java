157fe8099a89c5d3ece048b7036a51f3c68ebbfc
Naomi <npaulmin@yahoo.Com>
2017-02-27 14:17:38 +0100

    creation MAIN + saisie nombre magique

---
+++ Nombre_Mot_Magique/src/com/Main.java
@@ -0,0 +1,15 @@
+package com;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		//Saisie du nombre magique à trouver
+		Scanner saisie1 = new Scanner(System.in);
+		System.out.println("Veuillez saisir un nombre : ");
+		int nombreMagique = saisie1.nextInt();
+		
+	}
+	
+	
+}
