11a93eda5ed8700c7ef07c2ca4ad35f4e14a4b58
Naomi <npaulmin@yahoo.Com>
2017-03-29 12:19:08 +0200

    ajout de commentaires

--- Nombre_Magique_Console/src/com/Fonctions.java
+++ Nombre_Magique_Console/src/com/Fonctions.java
@@ -14,21 +14,25 @@ public class Fonctions {
 	int mode;
 	
 	public void choixNombreMagique() {
-		this.limiteJeu = this.limiteRand - 1;
-		this.nombreMagique = (int) (Math.random()*limiteRand);
+		this.limiteJeu = this.limiteRand - 1; //limite du jeu affichée au joueur
+		this.nombreMagique = (int) (Math.random()*limiteRand); //calcul du nombre magique
 	}
 	
 	//inclus choixNombreMagique & choixModeNombre
 	public void choixJeu() {
+		//choix du jeu
 		Scanner saisie = new Scanner(System.in);
 		System.out.println("Nombre magique : 1 / Mot magique : 2");
 		this.jeu = saisie.nextInt();
 		
+		//nombre magique
 		if(this.jeu == 1) {
 			choixNombreMagique();
 			choixModeNombre();
 
 		}
+		
+		//mot magique
 		else if(this.jeu == 2) {
 			System.out.println("JEU EN CONSTRUCTION");
 		}
@@ -36,9 +40,10 @@ public class Fonctions {
 	
 	//inclus nombreModeIllimite & nombreModeLimite
 	public void choixModeNombre() {
-		
+		//énoncé de la règle du jeu
 		System.out.println("Vous devez trouver le nombre magique compris entre 0 et " + this.limiteJeu + ".");
 		
+		//choix du mode de jeu
 		Scanner saisie = new Scanner(System.in);
 		System.out.println("Mode illimité : 1 / Mode limité : 2");
 		this.mode = saisie.nextInt();
@@ -58,7 +63,7 @@ public class Fonctions {
 	
 	//inclus verifNombreSaisi
 	public void verifLimiteJeu() {
-		//Verification nombre saisi compris dans les limites du random
+		//Verification nombre saisi compris dans les limites du jeu
 		if (this.nombreSaisi > limiteJeu || this.nombreSaisi < 0) {
 			Scanner saisie2 = new Scanner(System.in);
 			System.out.println("\n" + "Veuillez saisir un nombre COMPRIS entre 0 et " + limiteJeu + " :");
@@ -70,12 +75,13 @@ public class Fonctions {
 	}
 	
 	public void verifNombreSaisi() {
-		//si nombre saisi plus grand que nombre magique
+		//si le nombre saisi est plus grand que le nombre magique
 		if (this.nombreSaisi > this.nombreMagique) {
 			
 			//affichage "plus petit"
 			System.out.println("\n" + "Plus petit.");
 			
+			//si dans le mode limité on affiche les tentatives restantes
 			if(this.mode == 2) {
 				this.tentativesRestantes = tentativesMax - this.tentative;
 				System.out.println("Il vous reste " + this.tentativesRestantes + " tentative(s).");
@@ -89,12 +95,13 @@ public class Fonctions {
 			this.tentative = this.tentative+1;	
 		}
 			
-		//si nombre saisi plus petit que nombre magique
+		//si le nombre saisi est plus petit que le nombre magique
 		else if (this.nombreSaisi < this.nombreMagique) {
 				
 			//affichage "plus grand"
 			System.out.println("\n" + "Plus grand.");
 			
+			//si dans le mode limité on affiche les tentatives restantes
 			if(this.mode == 2) {
 				this.tentativesRestantes = tentativesMax - this.tentative;
 				System.out.println("Il vous reste " + this.tentativesRestantes + " tentative(s).");
@@ -125,22 +132,26 @@ public class Fonctions {
 	
 	//inclus saisieNombre & verifLimiteJeu
 	public void nombreModeLimite() {
-		
+		//énoncé de la règle du mode limité
 		System.out.println("\n" + "Vous avez droit à " + this.tentativesMax + " tentatives.");
 
 		saisieNombre();
 		this.tentative = 1;
 		
+		//on vérifie le nombre saisi et le nombre de tentatives du joueur
 		while (this.nombreSaisi != this.nombreMagique && this.tentative != this.tentativesMax) {
 			verifLimiteJeu();
 		}
 		
+		//si le nombre magique est trouvé
 		if (this.nombreSaisi == this.nombreMagique || (this.nombreSaisi == this.nombreMagique && this.tentative == this.tentativesMax)) {
 			//affichage du résultat 
 			System.out.println("\n" + "GAGNE :)");
 			System.out.println("Le nombre magique est " + this.nombreMagique); //affichage nombre magique
 			System.out.println("Nombre de tentatives : " + this.tentative); //affichage nombre tentative
 		}
+		
+		//si le nombre magique n'est pas trouvé
 		else if (this.tentative == tentativesMax){
 			System.out.println("\n" + "PERDU :(");
 			System.out.println("Le nombre magique était " + this.nombreMagique); //affichage nombre magique
--- Nombre_Magique_Console/src/com/Main.java
+++ Nombre_Magique_Console/src/com/Main.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Fonctions jeu = new Fonctions();
-		jeu.choixJeu();	
+		jeu.choixJeu();	//lancement du jeu
 	}	
 }
